03c84c19f3f55874561b11c8d898091b210d2df8
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-12-12 18:55:28 +0000

    feat: add appropriate css class based on the current time

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,14 @@ let storedTasks = [];
 
 // Display current date below title, standard format
 $("#currentDay").text(currentDay);
+
+// Add appropriate css class based on the current time
+$(".task-block").each(() => {
+  if (parseInt(currentHour) === parseInt(this.id)) {
+    $(this).addClass("present");
+  } else if (parseInt(currentHour) > parseInt(this.id)) {
+    $(this).addClass("past");
+  } else {
+    $(this).addClass("future");
+  }
+});
