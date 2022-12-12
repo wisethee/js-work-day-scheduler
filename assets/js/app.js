b3f048175f686b78a58e8499df5300ce45671889
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-12-12 18:59:00 +0000

    feat: add appropriate css class to save task based on the current time

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ let storedTasks = [];
 // Display current date below title, standard format
 $("#currentDay").text(currentDay);
 
-// Add appropriate css class based on the current time
+// Add appropriate css class to task block based on the current time
 $(".task-block").each(() => {
   if (parseInt(currentHour) === parseInt(this.id)) {
     $(this).addClass("present");
@@ -14,5 +14,16 @@ $(".task-block").each(() => {
     $(this).addClass("past");
   } else {
     $(this).addClass("future");
+  }
+});
+
+// Add appropriate css class to save task based on the current time
+$(".save-task").each((index, record) => {
+  if (parseInt(currentHour) === parseInt(record.id.split("-")[1])) {
+    $(record).addClass("present");
+  } else if (parseInt(currentHour) > parseInt(record.id.split("-")[1])) {
+    $(record).addClass("past");
+  } else {
+    $(record).addClass("future");
   }
 });
